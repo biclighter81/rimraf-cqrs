86bed8a49dc6fcbf7d62e6717851578d704e5456
andreas <petzi84@gmx.net>
2023-10-30 15:00:14 +0000

    -

--- apps/read-model/src/ReadModel.ts
+++
@@ -1,9 +0,0 @@
-import { EventHandlerFunc, Event } from "rimraf-cqrs-lib";
-
-export interface QueueReducer { (event: Event<unknown>): Promise<void> };
-
-export interface ReadModelList {
-  ListName: string
-
-  AggregateReducer(aggName: string): QueueReducer | undefined
-}
